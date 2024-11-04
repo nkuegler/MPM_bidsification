bb3cfc8907193b205835acb3425681dc87daf19e
Niklas Kuegler <kuegler@cbs.mpg.de>
2024-11-04 14:05:43 +0100

    Fixed bug of not finding DWI files if there is no trailing slash / at the end of the path

--- main_dicom_conv.sh
+++ main_dicom_conv.sh
@@ -1,33 +1,52 @@
-#!/bin/sh
 #!/bin/bash
 
-# $1: input dir
-# $2: output dir
+# $1: input_dir
+# $2: output_dir
+
+function adjust_path {
+    # pass a string (path) as argument
+    # check if the last character in the string is a '/'
+    # if not, add a '/' to the end
+    # return the updated string
+    path_string=$1
+    if [[ "${path_string: -1}" != "/" ]]; then
+        path_string="${path_string}/"
+    fi
+    echo $path_string
+}
 
 
-mkdir tmp
+# if paths are not defined with '/' at the end, the find command will not work recursively
+input_dir=$(adjust_path $1)
+output_dir=$(adjust_path $2)
 
+tmp_dir="tmp2"
+
+if [[ ! -d $tmp_dir ]]; then
+    mkdir $tmp_dir
+fi
 
 ### defining data to be converted using dcm2niix instead of hMRI Dicom Convert (recommendation: diffusion data)
 hMRI_dcmConv_excl="noddi"
 
 ### converting DICOMs to Niftis using hMRI toolbox (excluding data in folders containing the exclusion string)
 
-matlab -nodesktop -nosplash -r "call_dicom_conversion('$1','$2','$hMRI_dcmConv_excl')"
+matlab -nodesktop -nosplash -r "call_dicom_conversion('$input_dir','$output_dir','$hMRI_dcmConv_excl')"
 echo ">>> hMRI DICOM Import done!"
 
 ### converting "excluded" data using dcm2niix
 echo ">>> starting dcm2niix conversion"
-find $1 -type d -name "*$hMRI_dcmConv_excl*" > tmp/excl_dirs.txt
+dirs_txt_fname=excl_dirs_$(date +"%Y%m%d_%H%M%S").txt
+find $input_dir -type d -name "*$hMRI_dcmConv_excl*" > $tmp_dir/$dirs_txt_fname
 
 
-if [ ! -s tmp/excl_dirs.txt ]; then
+if [[ ! -s $tmp_dir/$dirs_txt_fname ]]; then
     echo "No directories found that contain the pattern '$hMRI_dcmConv_excl'"
 else
     while IFS= read -r directory; do
-        dcm2niix -o $2 -ba y -b y -f %p_%4s/s%t-%e -z n $directory; 
-    done < tmp/excl_dirs.txt
+        dcm2niix -o $output_dir -ba y -b y -f %p_%4s/s%t-%e -z n $directory; 
+    done < $tmp_dir/$dirs_txt_fname
     echo ">>> dcm2niix conversion done"
 fi
 
-rm -rf tmp
+rm -rf $tmp_dir

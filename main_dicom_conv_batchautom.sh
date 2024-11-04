33b0fe16dd1e32d47563756eb07f958572e1dece
Niklas Kuegler <kuegler@cbs.mpg.de>
2024-11-04 14:08:37 +0100

    Fixed bugs in the automization of Dicom Conversion
    Syntax and shebang from sh to bash. Adjusted the way how the paths are defined in the settings file. Fixed errors in BASH script according to the new way of path definition in the settings.

--- main_dicom_conv_batchautom.sh
+++ main_dicom_conv_batchautom.sh
@@ -1,56 +1,90 @@
-#!/bin/sh
 #!/bin/bash
 
+# change directory to directory of this script to allow it being called from anywhere
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 
 source settings.py
 
-if [$run_dicom_conv = '1']; then
+
+function adjust_path {
+    # Function: adjust_path
+    # Description: Adjusts the given path string by ensuring it ends with a '/'.
+    # Arguments:
+    #   $1 - A string representing the path to be adjusted.
+    # Returns:
+    #   The adjusted path string with a trailing '/' if it was not already present.
+
+    path_string=$1
+    if [[ "${path_string: -1}" != "/" ]]; then
+        path_string="${path_string}/"
+    fi
+    echo $path_string
+}
+
+
+# variable name where the last path for dcm and nii are stored
+last_dcm_path_name="path_to_dcm${number_of_paths}"
+last_nii_path_name="path_to_nii${number_of_paths}"
+
+tmp_dir="tmp"
+
+if [[ $run_dicom_conv = "1" ]]; then
+    echo ">>>> Running Dicom to Nifti Conversion"
     echo ">>>> using the variables defined in settings.py"
 
-    if [ ${#path_to_dcm[@]} -ne ${#path_to_nii[@]} ]; then
-        echo "Error: The lists path_to_dcm and path_to_nii must have the same length." >&2
+    if [[ -z ${!last_dcm_path_name} ]] || [[ -z ${!last_nii_path_name} ]]; then
+        echo "Error: There must be as many path_to_dcm as path_to_nii specified in the settings file." #>&2
         exit 1
+    else
+        echo ">>>> Number of directories to convert: ${number_of_paths}"
     fi
 
-    for i in "${!path_to_dcm[@]}"; do
+    if [[ ! -d $tmp_dir ]]; then
+        mkdir $tmp_dir
+    fi
 
-        if [ ! -d tmp]; then
-            mkdir tmp
-        fi
+    for i in $(seq 1 $number_of_paths); do
+        echo "---------------------------------------"
+        
+        curr_path_to_dcm="path_to_dcm${i}"
+        curr_path_to_nii="path_to_nii${i}"
+
+        # if paths are not defined with '/' at the end, the find command will not work recursively
+        curr_path_to_dcm=$(adjust_path ${!curr_path_to_dcm})
+        curr_path_to_nii=$(adjust_path ${!curr_path_to_nii})
 
         # Create output directory if not present
-        if [ ! -d $path_to_nii[$i]]; then
-            mkdir $path_to_nii[$i]
+        if [[ ! -d $curr_path_to_nii ]]; then
+            mkdir $curr_path_to_nii
         fi
 
         ### converting DICOMs to Niftis using hMRI toolbox (excluding data in folders containing the exclusion string)
-        matlab -nodesktop -nosplash -r "call_dicom_conversion('$path_to_dcm[$i]','$path_to_nii[$i]','$hMRI_dcmConv_excl')"
+        matlab -nodesktop -nosplash -r "call_dicom_conversion('$curr_path_to_dcm','$curr_path_to_nii','$hMRI_dcmConv_excl')"
         echo ">>> hMRI DICOM Import done!"
 
         ### converting "excluded" data using dcm2niix
         echo ">>> starting dcm2niix conversion"
-        find $path_to_dcm[$i] -type d -name "*$hMRI_dcmConv_excl*" > tmp/excl_dirs.txt
+        dirs_txt_fname=excl_dirs_$(date +"%Y%m%d_%H%M%S").txt
+        find $curr_path_to_dcm -type d -name "*$hMRI_dcmConv_excl*" > $tmp_dir/$dirs_txt_fname
 
 
-        if [ ! -s tmp/excl_dirs.txt ]; then
+        if [[ ! -s $tmp_dir/$dirs_txt_fname ]]; then
             echo ">>>> No directories found that contain the pattern '$hMRI_dcmConv_excl'. dcm2niix conversion skipped."
         else
             while IFS= read -r directory; do
-                dcm2niix -o $path_to_nii[$i] -ba y -b y -f %p_%4s/s%t-%e -z n $directory; 
-            done < tmp/excl_dirs.txt
+                dcm2niix -o $curr_path_to_nii -ba y -b y -f %p_%4s/s%t-%e -z n $directory; 
+            done < $tmp_dir/$dirs_txt_fname
             echo ">>> dcm2niix conversion done"
         fi
 
-        rm -rf tmp
+        rm -f $tmp_dir/$dirs_txt_fname
     
     done
+
+    rm -rf $tmp_dir
 
 
 else
     echo ">>>> dicom conversion skipped"
 fi
-
-
-
-
-
--- settings.py
+++ settings.py
@@ -3,21 +3,30 @@
 
 ############### Conversion from Dicoms to Nifti using hMRI Dicom Import and dcm2niix ###############
 run_dicom_conv=1
-path_to_dcm=['/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/dcm',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/dcm',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/dcm',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/dcm',\
-]
-path_to_nii=['/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/nii2',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/nii2',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/nii2',\
-              '/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/nii2',\
-]
+
+# defining the path to the dicom data and the path to the nifti data
+# bash cannot handle lists of strings, thus we need to define each path separately
+# number_of_paths specifies the last directory to be processed (e.g. 4 means 4 path_to_dcm4 and path_to_nii4)
+number_of_paths=4
+
+# start the index at 1
+path_to_dcm1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/dcm"
+path_to_nii1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/nii"
+
+path_to_dcm2="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/dcm"
+path_to_nii2="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/nii"
+
+path_to_dcm3="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/dcm"
+path_to_nii3="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/nii"
+
+path_to_dcm4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/dcm"
+path_to_nii4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/nii"
+
 
 # defining data to be converted using dcm2niix instead of hMRI Dicom Convert (recommendation: diffusion data)
 hMRI_dcmConv_excl='noddi'
 
 
 ############### Bidsification of the data set ###############
-run_bidsification_from_nii=1
+run_bidsification_from_nii=0
 path_to_nii_bids=[]

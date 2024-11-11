e3536f6f3098323777567b4891cf978b924a08ab
Niklas Kuegler <kuegler@cbs.mpg.de>
2024-11-11 18:08:18 +0100

    Small adjustments to settings and bidsme data preparation.

--- main_dicom_conv_batchautom.sh
+++ main_dicom_conv_batchautom.sh
@@ -33,8 +33,8 @@ if [[ $run_dicom_conv = "1" ]]; then
     echo ">>>> Running Dicom to Nifti Conversion"
     echo ">>>> using the variables defined in settings.py"
 
-    if [[ -z ${!last_dcm_path_name} ]] || [[ -z ${!last_nii_path_name} ]]; then
-        echo "Error: There must be as many path_to_dcm as path_to_nii specified in the settings file." #>&2
+    if [[ -z ${!last_dcm_path_name} ]] || [[ -z ${!last_nii_path_name} ]]; then         ## TODO: loop over all integers until number_of_paths and check if dcm and nii are specified.
+        echo "Error: There must be as many path_to_dcm as path_to_nii specified in the settings file." #>&2     ## best to include in loop further down
         exit 1
     else
         echo ">>>> Number of directories to convert: ${number_of_paths}"
--- settings.py
+++ settings.py
@@ -7,21 +7,45 @@ run_dicom_conv=1
 # defining the path to the dicom data and the path to the nifti data
 # bash cannot handle lists of strings, thus we need to define each path separately
 # number_of_paths specifies the last directory to be processed (e.g. 4 means 4 path_to_dcm4 and path_to_nii4)
-number_of_paths=4
+number_of_paths=7
 
 # start the index at 1
-path_to_dcm1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/dcm"
-path_to_nii1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/nii"
+# subject 1
+path_to_dcm1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230505/dcm"
+path_to_nii1="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230505/nii"
 
-path_to_dcm2="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/dcm"
-path_to_nii2="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/nii"
+# path_to_dcm2="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230511/dcm"
+# path_to_nii2="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230511/nii"
 
-path_to_dcm3="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/dcm"
-path_to_nii3="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/nii"
+# path_to_dcm3="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230512/dcm"
+# path_to_nii3="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20230512/nii"
 
-path_to_dcm4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/dcm"
-path_to_nii4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/nii"
+path_to_dcm2="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/dcm"
+path_to_nii2="/data/pt_02262/bids_test/bids_folders_autom/source/37446.6e/20240222/nii"
 
+# subject 2
+# path_to_dcm5="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20230511/dcm"
+# path_to_nii5="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20230511/nii"
+
+# path_to_dcm6="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20230512/dcm"
+# path_to_nii6="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20230512/nii"
+
+path_to_dcm3="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/dcm"
+path_to_nii3="/data/pt_02262/bids_test/bids_folders_autom/source/40851.ff/20240403/nii"
+
+# subject 3
+path_to_dcm4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20230606/dcm"
+path_to_nii4="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20230606/nii"
+
+path_to_dcm5="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/dcm"
+path_to_nii5="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231121/nii"
+
+path_to_dcm6="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/dcm"
+path_to_nii6="/data/pt_02262/bids_test/bids_folders_autom/source/41006.a1/20231123/nii"
+
+# subject 4
+path_to_dcm7="/data/pt_02262/bids_test/bids_folders_autom/source/41486.2c/20230928/dcm"
+path_to_nii7="/data/pt_02262/bids_test/bids_folders_autom/source/41486.2c/20230928/nii"
 
 # defining data to be converted using dcm2niix instead of hMRI Dicom Convert (recommendation: diffusion data)
 hMRI_dcmConv_excl='noddi'

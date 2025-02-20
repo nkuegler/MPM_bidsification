5db24c354eb283207387da6dcc2ff24bc678d40c
Niklas Kuegler <kuegler@cbs.mpg.de>
2025-02-20 12:01:19 +0100

    Adjust hMRI-toolbox to IronSleep version. Re-run DICOM-to-NIfTI conversion

--- call_dicom_conversion.m
+++ call_dicom_conversion.m
@@ -19,7 +19,7 @@ function call_dicom_conversion(input_dir, output_dir, excl_str, check_results)
     % end
 
     addpath('/data/u_kuegler_software/git/spm12')
-    addpath('/data/u_kuegler_software/git/hMRI-toolbox')
+    addpath('/data/u_kuegler_software/git/hMRI-toolbox_IronSleep')
     addpath('/data/u_kuegler_software/git/MPM_bidsification/hMRI_toolbox_functions/dicom_import')
 
     disp("Retrieving DICOMs from " + input_dir)
--- settings.py
+++ settings.py
@@ -7,45 +7,48 @@ run_dicom_conv=1
 # defining the path to the dicom data and the path to the nifti data
 # bash cannot handle lists of strings, thus we need to define each path separately
 # number_of_paths specifies the last directory to be processed (e.g. 4 means 4 path_to_dcm4 and path_to_nii4)
-number_of_paths=2
+number_of_paths=12
 
 # start the index at 1
-# subject 1
-# path_to_dcm1="/data/pt_02262/data/TH_bids/source/37446.6e/20230505/dcm"
-# path_to_nii1="/data/pt_02262/data/TH_bids/source/37446.6e/20230505/nii"
+# subject 37446.6e
+path_to_dcm1="/data/pt_02262/data/TH_bids/source/37446.6e/20230505/dcm"
+path_to_nii1="/data/pt_02262/data/TH_bids/source/37446.6e/20230505/nii"
 
-# path_to_dcm2="/data/pt_02262/data/TH_bids/source/37446.6e/20230511/dcm" ## not working atm
-# path_to_nii2="/data/pt_02262/data/TH_bids/source/37446.6e/20230511/nii"
+path_to_dcm2="/data/pt_02262/data/TH_bids/source/37446.6e/20230511/dcm" ## not working atm (?)
+path_to_nii2="/data/pt_02262/data/TH_bids/source/37446.6e/20230511/nii"
 
-# path_to_dcm3="/data/pt_02262/data/TH_bids/source/37446.6e/20230512/dcm"
-# path_to_nii3="/data/pt_02262/data/TH_bids/source/37446.6e/20230512/nii"
+path_to_dcm3="/data/pt_02262/data/TH_bids/source/37446.6e/20230512/dcm"
+path_to_nii3="/data/pt_02262/data/TH_bids/source/37446.6e/20230512/nii"
 
-# path_to_dcm4="/data/pt_02262/data/TH_bids/source/37446.6e/20240222/dcm"
-# path_to_nii4="/data/pt_02262/data/TH_bids/source/37446.6e/20240222/nii"
+path_to_dcm4="/data/pt_02262/data/TH_bids/source/37446.6e/20240222/dcm"
+path_to_nii4="/data/pt_02262/data/TH_bids/source/37446.6e/20240222/nii"
 
-# subject 2
-path_to_dcm1="/data/pt_02262/data/TH_bids/source/40851.ff/20230511/dcm"
-path_to_nii1="/data/pt_02262/data/TH_bids/source/40851.ff/20230511/nii"
+# subject 40851.ff
+path_to_dcm5="/data/pt_02262/data/TH_bids/source/40851.ff/20230505/dcm"
+path_to_nii5="/data/pt_02262/data/TH_bids/source/40851.ff/20230505/nii"
 
-path_to_dcm2="/data/pt_02262/data/TH_bids/source/40851.ff/20230512/dcm"
-path_to_nii2="/data/pt_02262/data/TH_bids/source/40851.ff/20230512/nii"
+path_to_dcm6="/data/pt_02262/data/TH_bids/source/40851.ff/20230511/dcm"
+path_to_nii6="/data/pt_02262/data/TH_bids/source/40851.ff/20230511/nii"
 
-# path_to_dcm7="/data/pt_02262/data/TH_bids/source/40851.ff/20240403/dcm"
-# path_to_nii7="/data/pt_02262/data/TH_bids/source/40851.ff/20240403/nii"
+path_to_dcm7="/data/pt_02262/data/TH_bids/source/40851.ff/20230512/dcm"
+path_to_nii7="/data/pt_02262/data/TH_bids/source/40851.ff/20230512/nii"
 
-# subject 3
-# path_to_dcm8="/data/pt_02262/data/TH_bids/source/41006.a1/20230606/dcm"
-# path_to_nii8="/data/pt_02262/data/TH_bids/source/41006.a1/20230606/nii"
+path_to_dcm8="/data/pt_02262/data/TH_bids/source/40851.ff/20240403/dcm"
+path_to_nii8="/data/pt_02262/data/TH_bids/source/40851.ff/20240403/nii"
 
-# path_to_dcm9="/data/pt_02262/data/TH_bids/source/41006.a1/20231121/dcm"
-# path_to_nii9="/data/pt_02262/data/TH_bids/source/41006.a1/20231121/nii"
+# subject 41006.a1
+path_to_dcm9="/data/pt_02262/data/TH_bids/source/41006.a1/20230606/dcm"
+path_to_nii9="/data/pt_02262/data/TH_bids/source/41006.a1/20230606/nii"
 
-# path_to_dcm10="/data/pt_02262/data/TH_bids/source/41006.a1/20231123/dcm"
-# path_to_nii10="/data/pt_02262/data/TH_bids/source/41006.a1/20231123/nii"
+path_to_dcm10="/data/pt_02262/data/TH_bids/source/41006.a1/20231121/dcm"
+path_to_nii10="/data/pt_02262/data/TH_bids/source/41006.a1/20231121/nii"
 
-# subject 4
-# path_to_dcm11="/data/pt_02262/data/TH_bids/source/41486.2c/20230928/dcm"
-# path_to_nii11="/data/pt_02262/data/TH_bids/source/41486.2c/20230928/nii"
+path_to_dcm11="/data/pt_02262/data/TH_bids/source/41006.a1/20231123/dcm"
+path_to_nii11="/data/pt_02262/data/TH_bids/source/41006.a1/20231123/nii"
+
+# subject 41486.2c
+path_to_dcm12="/data/pt_02262/data/TH_bids/source/41486.2c/20230928/dcm"
+path_to_nii12="/data/pt_02262/data/TH_bids/source/41486.2c/20230928/nii"
 
 # defining data to be converted using dcm2niix instead of hMRI Dicom Convert (recommendation: diffusion data)
 hMRI_dcmConv_excl='noddi' # case invariant comparison

3a81cfd1343ab0536eb3ba7d782c782650a97e0c
Niklas Kuegler <kuegler@cbs.mpg.de>
2025-03-02 22:22:18 +0100

    Adjust path of dicom_import directory

--- call_dicom_conversion.m
+++ call_dicom_conversion.m
@@ -20,7 +20,7 @@ function call_dicom_conversion(input_dir, output_dir, excl_str, check_results)
 
     addpath('/data/u_kuegler_software/git/spm12')
     addpath('/data/u_kuegler_software/git/hMRI-toolbox_IronSleep')
-    addpath('/data/u_kuegler_software/git/MPM_bidsification/hMRI_toolbox_functions/dicom_import')
+    addpath('/data/u_kuegler_software/git/MPM_bidsification/spm_dicom_import')
 
     disp("Retrieving DICOMs from " + input_dir)
 

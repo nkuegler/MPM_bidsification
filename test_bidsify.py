bc83a07a32cf3746d956c65f7bf0f0934496645b
Friedrich Krohn <>
2026-06-19 13:39:42 +0200

    added automatization test files

--- test_bidsify.py
+++ test_bidsify.py
@@ -53,7 +53,7 @@ bidsme.prepare(str(SOURCE_PATH), str(PREPARED_PATH),
                part_template=str(WORKING_DIR / "supplementary" / "table_templates" / "participants_nk.json"),
                plugin_opt={"sessions_tsv_template": str(
                    WORKING_DIR / "supplementary" / "table_templates" / "sessions_nk.json")},
-               sub_list=["sub-014","sub-015","sub-017"] # only run on specified subjects (must be specified in BIDS notation)
+               sub_list=["sub-004","sub-008","sub-019"] # only run on specified subjects (must be specified in BIDS notation)
                )
 bidsme.tools.info.reporterrors(logger)
 bidsme.tools.info.reseterrors(logger)
@@ -64,7 +64,7 @@ PLUGIN_BIDS = WORKING_DIR/"plugins_bidsme/plugin_bidsify_auto_nk_rewritten_FIXED
 
 bidsme.mapper(str(PREPARED_PATH), str(BIDSIFIED_PATH), plugin_file=str(PLUGIN_BIDS),
               plugin_opt={"bidsmap_step": True},
-              sub_list=["sub-014","sub-015","sub-017"],
+              sub_list=["sub-004","sub-008","sub-019"],
               )
 bidsme.tools.info.reporterrors(logger)
 bidsme.tools.info.reseterrors(logger)
@@ -74,7 +74,7 @@ MAP_FILE = str(BIDSIFIED_PATH / "code" / "bidsme" / "bidsmap.yaml")
 PLUGIN_FILE_BIDS = str(WORKING_DIR / "plugins_bidsme" / "plugin_bidsify_auto_nk.py")
 
 import subprocess
-subjects = ["sub-014","sub-015","sub-017"]
+subjects = ["sub-004","sub-008","sub-019"]
 
 subprocess.run([
     "bidsme", "bidsify",
--- test_bidsify_loraks.py
+++ test_bidsify_loraks.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 import bidsme
 
+from test_bidsify import subjects
+
 # RESOURCES_PATH = os.path.join(DATASET_PATH, "example1", "resources")
 
 
@@ -35,35 +37,34 @@ bidsme.prepare(str(SOURCE_PATH), str(PREPARED_PATH),
                part_template = str(WORKING_DIR / "supplementary" / "table_templates" / "participants_nk.json"),
                plugin_opt = {"sessions_tsv_template": str(WORKING_DIR / "supplementary" / "table_templates" / "sessions_nk.json"), "include_smaps": False},
                # sub_skip_dir=True,
-               sub_list=["sub-020"] # only run on specified subjects (must be specified in BIDS notation)
+               sub_list=["sub-004","sub-008","sub-019"]  # only run on specified subjects (must be specified in BIDS notation)
               )
 bidsme.tools.info.reporterrors(logger)
 bidsme.tools.info.reseterrors(logger)
 
-PLUGIN_BIDS = WORKING_DIR / "plugins_bidsme" / "plugin_bidsify_auto_nk.py"
+PLUGIN_BIDS = WORKING_DIR / "plugins_bidsme" / "plugin_prepare_unified_patched_FIXED.py"
 # PLUGIN_BIDS = WORKING_DIR / "plugins_bidsme" / "liege_data_IronSleep" / "plugin_bidsify_loraks_liegeData_nk.py"
 
 bidsme.mapper(str(PREPARED_PATH), str(BIDSIFIED_PATH), plugin_file=str(PLUGIN_BIDS),
               plugin_opt={"bidsmap_step": True, "include_smaps": False},
-              sub_list=["sub-20"]
+              sub_list=["sub-004","sub-008","sub-019"],
               # sub_skip_tsv=True,
               )
 bidsme.tools.info.reporterrors(logger)
 bidsme.tools.info.reseterrors(logger)
 
 MAP_FILE = str(BIDSIFIED_PATH / "code" / "bidsme" / "bidsmap.yaml")
-PLUGIN_FILE_BIDS = str(WORKING_DIR / "plugins_bidsme" / "plugin_bidsify_loraks_nk.py")
+PLUGIN_FILE_BIDS = str(WORKING_DIR / "plugins_bidsme" / "plugin_bidsify_auto_nk_rewritten_FIXED.py")
 # PLUGIN_FILE_BIDS = WORKING_DIR / "plugins_bidsme" / "liege_data_IronSleep" / "plugin_bidsify_loraks_liegeData_nk.py"
 
 import subprocess
-
+subjets=["sub-004","sub-008","sub-019"]
 subprocess.run([
     "bidsme", "bidsify",
     str(PREPARED_PATH),
     str(BIDSIFIED_PATH),
     "-b", str(MAP_FILE),
     "--plugin", str(PLUGIN_FILE_BIDS),
-    "--participants", "sub-020",
-    "--skip-existing",
+    "--participants", *subjets,
 ], check=True)# !bidsme bidsify $PREPARED_PATH $BIDSIFIED_PATH -b $MAP_FILE --plugin $PLUGIN_FILE_BIDS --participants 'sub-001'
 # !bidsme bidsify $PREPARED_PATH $BIDSIFIED_PATH -b $MAP_FILE --plugin $PLUGIN_FILE_BIDS --skip-existing
